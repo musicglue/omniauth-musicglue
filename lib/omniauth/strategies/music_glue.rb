1cfd3f5d4eced5edce3b5630ae2af6d95ea4ad70
Adam Carlile <adam@benchmedia.co.uk>
2014-06-04 11:45:12 +0100

    Updated to use doorkeeper

--- lib/omniauth/strategies/music_glue.rb
+++ lib/omniauth/strategies/music_glue.rb
@@ -2,14 +2,29 @@ require 'omniauth-oauth2'
 
 module OmniAuth
   module Strategies
-    class MusicGlue < OmniAuth::Strategies::OAuth2
+    class Doorkeeper < OmniAuth::Strategies::OAuth2
       BaseAuthUrl = ENV["MUSIC_GLUE_AUTH_URL"] || "https://id.musicglue.com"
+
+      # change the class name and the :name option to match your application name
+      option :name, :doorkeeper
 
       option :client_options, {
         :site => BaseAuthUrl,
-        :authorize_url => "#{BaseAuthUrl}/oauth/authorize",
-        :token_url => "#{BaseAuthUrl}/oauth/token"
+        :authorize_url => "/oauth/authorize"
       }
+
+      uid { raw_info["id"] }
+
+      info do
+        {
+          :email => raw_info["email"]
+          # and anything else you want to return to your API consumers
+        }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get('/me.json').parsed
+      end
     end
   end
 end
