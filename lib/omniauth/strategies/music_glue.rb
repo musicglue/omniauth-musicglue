6f4364e79190a2334d6c0811afbae948d75b5ce6
Adam Carlile <adam@benchmedia.co.uk>
2014-06-04 11:51:36 +0100

    Oops

--- lib/omniauth/strategies/music_glue.rb
+++ lib/omniauth/strategies/music_glue.rb
@@ -2,11 +2,11 @@ require 'omniauth-oauth2'
 
 module OmniAuth
   module Strategies
-    class Doorkeeper < OmniAuth::Strategies::OAuth2
+    class MusicGlue < OmniAuth::Strategies::OAuth2
       BaseAuthUrl = ENV["MUSIC_GLUE_AUTH_URL"] || "https://id.musicglue.com"
 
       # change the class name and the :name option to match your application name
-      option :name, :doorkeeper
+      option :name, :music_glue
 
       option :client_options, {
         :site => BaseAuthUrl,
